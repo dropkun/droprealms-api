483c94a6cdd1b8728f36526578436c694e59cfd8
dropkun <junya111226@gmail.com>
2024-01-31 23:01:51 +0900

    added get instance method with comment

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 mod error;
 
 use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Response},
     routing::{get, post},
     Json, Router,
 };
 use error::AppError;
 use reqwest::blocking::Client;
 use serde::{Deserialize, Serialize};
-
-#[derive(Serialize)]
-struct Instance {
-    instance_name: String,
-    project: String,
-    zone: String,
-}
+use std::collections::HashMap;
+use std::env;
 
 #[derive(Debug, Deserialize)]
 struct TokenResponse {
@@ -30,6 +23,9 @@ struct InstanceParam {
     project: String,
     zone: String,
 }
+
+#[derive(Debug, Deserialize)]
+struct GetInstanceParam {}
 
 async fn get_metadata_token() -> Result<String, reqwest::Error> {
     // GCP metadata endpoint URL
@@ -46,6 +42,23 @@ async fn get_metadata_token() -> Result<String, reqwest::Error> {
     let token_response: TokenResponse = serde_json::from_str(&json_string).unwrap();
     Ok(token_response.access_token)
 }
+
+async fn send_discord_webhook(msg: String) -> Result<(), reqwest::Error> {
+    let webhook_url = env::var("DISCORD_WEBHOOK").expect("Expected a url in the environment");
+
+    let mut map = HashMap::new();
+    map.insert("content", msg);
+    map.insert("username", String::from("droprealms-api"));
+    map.insert("avatar_url", String::from("https://github.com/google.png"));
+
+    let client = reqwest::Client::new();
+    let response = client.post(webhook_url).json(&map).send().await?;
+
+    println!("Response: {:?}", response);
+
+    Ok(())
+}
+
 async fn stop_instance(Json(payload): Json<InstanceParam>) -> Result<(), AppError> {
     let client = Client::new();
     let url = format!(
@@ -62,9 +75,10 @@ async fn stop_instance(Json(payload): Json<InstanceParam>) -> Result<(), AppErro
         .header("Authorization", format!("Bearer {}", token))
         .send()?;
 
-    // レスポンスの処理
-    println!("Status: {:?}", response.status());
+    println!("Status: {}", response.status());
     println!("Body: {:?}", response.text());
+
+    send_discord_webhook(format!("{} was stopped to shutdown.", payload.name)).await?;
     Ok(())
 }
 
@@ -84,12 +98,41 @@ async fn start_instance(Json(payload): Json<InstanceParam>) -> Result<(), AppErr
         .header("Authorization", format!("Bearer {}", token))
         .send()?;
 
-    // レスポンスの処理
-    println!("Status: {:?}", response.status());
+    println!("Status: {}", response.status());
     println!("Body: {:?}", response.text());
+
+    send_discord_webhook(format!("{} was started to boot.", payload.name)).await?;
 
     Ok(())
 }
+
+// async fn get_instance(Json(payload): Json<InstanceParam>) -> Result<String, AppError> {
+//     let url = format!(
+//         "https://compute.googleapis.com/compute/v1/projects/{}/zones/{}/instances/{}",
+//         payload.project, payload.zone, payload.name
+//     );
+
+//     let token = get_metadata_token().await?;
+
+//     let response = reqwest::Client::new()
+//         .get(&url)
+//         .header("Authorization", format!("Bearer {}", token))
+//         .send()
+//         .await?;
+
+//     send_discord_webhook(format!("{} was started to boot.", payload.name)).await?;
+
+//     if response.status().is_success() {
+//         let instance_info: Instance = response.json().await?;
+//         Ok(Json(instance_info))
+//     } else {
+//         // レスポンスが成功でない場合、エラーメッセージを含んだAppErrorを返す
+//         Err(AppError(anyhow::Error::msg(format!(
+//             "Failed to retrieve instance information for {}",
+//             payload.name
+//         ))))
+//     }
+// }
 
 async fn root() -> &'static str {
     "Welcome to droprealms!"
