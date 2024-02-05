c8a0021e111bf9e7d8ab6d465262a77c1e4c8097
dropkun <junya111226@gmail.com>
2024-02-05 21:24:21 +0900

    refactored

--- src/handler.rs
+++ src/handler.rs
@@ -6,32 +6,34 @@ use axum::Json;
 use error::AppError;
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
-pub struct InstanceParam {
+use utoipa::ToSchema;
+
+#[derive(Debug, Deserialize, ToSchema)]
+pub struct InstanceRequest {
     name: String,
     project: String,
     zone: String,
 }
 
-pub async fn stop_instance(Json(payload): Json<InstanceParam>) -> Result<(), AppError> {
+pub async fn stop_instance(Json(payload): Json<InstanceRequest>) -> Result<(), AppError> {
     gcp::stop_instance(&payload.name, &payload.project, &payload.zone).await?;
     notify::send_discord_webhook(format!("{} was started to shutdown.", payload.name)).await?;
     anyhow::Result::Ok(())
 }
 
-pub async fn start_instance(Json(payload): Json<InstanceParam>) -> Result<(), AppError> {
+pub async fn start_instance(Json(payload): Json<InstanceRequest>) -> Result<(), AppError> {
     gcp::start_instance(&payload.name, &payload.project, &payload.zone).await?;
     notify::send_discord_webhook(format!("{} was started to boot.", payload.name)).await?;
     anyhow::Result::Ok(())
 }
 
-pub async fn get_ip(Json(payload): Json<InstanceParam>) -> Result<String, AppError> {
+pub async fn get_ip(Json(payload): Json<InstanceRequest>) -> Result<String, AppError> {
     let ip = gcp::get_ip(&payload.name, &payload.project, &payload.zone).await?;
     notify::send_discord_webhook(String::from("Instance information obtained.")).await?;
     anyhow::Result::Ok((ip))
 }
 
-pub async fn get_status(Json(payload): Json<InstanceParam>) -> Result<String, AppError> {
+pub async fn get_status(Json(payload): Json<InstanceRequest>) -> Result<String, AppError> {
     let status = gcp::get_status(&payload.name, &payload.project, &payload.zone).await?;
     anyhow::Result::Ok((status))
 }
